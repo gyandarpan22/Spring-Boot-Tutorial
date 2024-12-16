f97f288d316cf362814caceb260a674d2777b9ec
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2024-12-16 18:30:29 +0530

    Add files via upload

---
+++ Connection Pooling in Spring Boot with Custom Configurations/rest-client-service/src/main/java/com/gyan/darpan/RestClientServiceApplication.java
@@ -0,0 +1,36 @@
+package com.gyan.darpan;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.gyan.darpan.model.Employee;
+import com.gyan.darpan.service.EmployeeService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class RestClientServiceApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(RestClientServiceApplication.class, args);
+
+        try {
+            EmployeeService employeeService = applicationContext.getBean(EmployeeService.class);
+
+            //employeeService.getEmployees();
+            // employeeService.getEmployee();
+            // employeeService.deleteEmployee();
+            System.out.print("Adding a new Employee----");
+            Employee employee = employeeService.addEmployee();
+
+            System.out.print("Fetching a new Employee detail----");
+            employeeService.getEmployee(employee.getEmployeeId());
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
