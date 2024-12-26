6176055424f7fa8a519a84ab7f8d6d77b07a7de2
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2024-12-26 19:25:33 +0530

    Add files via upload

---
+++ Rolling File Appender in Spring Boot_ Step-by-Step Tutorial/employee-service/src/main/java/com/gyan/darpan/employee/controller/DemoController.java
@@ -0,0 +1,67 @@
+package com.gyan.darpan.employee.controller;
+
+import com.gyan.darpan.employee.annotation.GlobalExceptionHandlerAnnotation1;
+import com.gyan.darpan.employee.exception.EmployeeNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@GlobalExceptionHandlerAnnotation1
+@Slf4j
+public class DemoController {
+
+    // private final Logger log = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping(path = "hello", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> sayHello() {
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "demo/{id}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> demo(@PathVariable int id) {
+        if (id > 100 || id < 0) {
+            throw new EmployeeNotFoundException(id);
+        }
+
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "demo/headers", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> headersDemo(@RequestHeader(name = "header1") String header) {
+        System.out.println("header1 : " + header);
+
+        String response = "header1:" + header;
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "demo/headers1", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> headersDemo1(@RequestHeader HttpHeaders header) {
+        log.info("header1 : {} ", header.getFirst("header1"));
+        log.debug("debug log");
+        log.warn("warn log");
+        log.error("error log");
+
+        for (Map.Entry<String, List<String>> entry : header.entrySet()) {
+            log.info("{} : {}", entry.getKey(), entry.getValue());
+        }
+
+        String response = "header1:" + header.getFirst("header1");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+}
