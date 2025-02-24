c9a44e2b5891e8df2a689fe84625dc93d7e07a55
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-02-24 15:42:40 +0530

    Add files via upload

---
+++ How to Load Custom Properties Files in Spring Boot/employee-service/src/main/java/com/gyan/darpan/employee/model/Address.java
@@ -0,0 +1,16 @@
+package com.gyan.darpan.employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+    private String street;
+    private String city;
+    private String country;
+}
