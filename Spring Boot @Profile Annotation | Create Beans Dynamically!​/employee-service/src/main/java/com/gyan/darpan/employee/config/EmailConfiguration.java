894122c6d1beb2497ea08c2baa351b8368737371
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-02-26 17:58:58 +0530

    Add files via upload

---
+++ Spring Boot @Profile Annotation | Create Beans Dynamically!​/employee-service/src/main/java/com/gyan/darpan/employee/config/EmailConfiguration.java
@@ -0,0 +1,25 @@
+package com.gyan.darpan.employee.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+//@PropertySource(value = "classpath:email-configuration.yaml", factory = YamlPropertySourceFactory.class)
+public class EmailConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "email.gmail")
+    public EmailConfig gmailConfig() {
+        return new EmailConfig();
+    }
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "email.outlook")
+    public EmailConfig outlookConfig() {
+        return new EmailConfig();
+    }
+
+}
