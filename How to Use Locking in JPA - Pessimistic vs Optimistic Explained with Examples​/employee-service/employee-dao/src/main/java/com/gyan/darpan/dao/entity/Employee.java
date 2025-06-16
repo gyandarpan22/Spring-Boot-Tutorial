42636bdff23cba3bda3d4b21a10f2ebb42c894cf
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-06-16 21:45:53 +0530

    Add files via upload

---
+++ How to Use Locking in JPA - Pessimistic vs Optimistic Explained with Examples​/employee-service/employee-dao/src/main/java/com/gyan/darpan/dao/entity/Employee.java
@@ -0,0 +1,46 @@
+package com.gyan.darpan.dao.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "employee")
+@NamedQuery(name = "Employee.findByEmployeeName",
+        query = "Select emp from Employee emp where emp.employeeName= :employeeName")
+public class Employee {
+    @Column(name = "employee_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employeeId;
+    @Column(name = "employee_name", nullable = false)
+    private String employeeName;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "joining_date", nullable = false)
+    private Date joiningDate;
+    @Column(name = "department", nullable = false)
+    private String department;
+    @Column(name = "created_date", nullable = false, insertable = false, updatable = false,
+            columnDefinition = "timestamp not null default current_timestamp")
+    private Timestamp createdDate;
+    @Column(name = "modified_date", nullable = false, insertable = false, updatable = false,
+            columnDefinition = "timestamp not null default current_timestamp on update current_timestamp")
+    private Timestamp modifiedDate;
+
+    public Employee(Long employeeId, String employeeName) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+    }
+}
