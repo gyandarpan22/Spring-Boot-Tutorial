296ee9060b7878c33b76700d9e244c63b702b0f9
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-06-17 22:40:29 +0530

    Add files via upload

---
+++ How to Use EntityManager to Update Data in Spring Boot​/employee-service/employee-api/src/main/java/com/gyan/darpan/employee/controller/DemoJdbcTemplateController.java
@@ -0,0 +1,223 @@
+package com.gyan.darpan.employee.controller;
+
+import com.gyan.darpan.dao.entity.DepartmentCount;
+import com.gyan.darpan.dao.entity.Employee;
+import com.gyan.darpan.dao.entity.PageResponse;
+import com.gyan.darpan.dao.repository.jdbc.DemoEntityManagerRepository;
+import com.gyan.darpan.dao.repository.jdbc.DemoJdbcTemplateRepository;
+import com.gyan.darpan.dao.repository.jdbc.DemoNamedParameterJdbcRepository;
+import com.gyan.darpan.employee.enums.Department;
+import com.gyan.darpan.employee.exception.EmployeeNotFoundException;
+import com.gyan.darpan.employee.mapper.EntityDTOMapper;
+import com.gyan.darpan.employee.model.EmployeeDTO;
+import com.gyan.darpan.employee.response.EmployeeListResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+public class DemoJdbcTemplateController {
+
+    private final DemoJdbcTemplateRepository demoJdbcTemplateRepository;
+    private final EntityDTOMapper<Employee, EmployeeDTO> employeeDTOEntityDTOMapper;
+    private final DemoNamedParameterJdbcRepository demoNamedParameterJdbcRepository;
+    private final DemoEntityManagerRepository demoEntityManagerRepository;
+
+    @Autowired
+    public DemoJdbcTemplateController(DemoJdbcTemplateRepository demoJdbcTemplateRepository,
+                                      @Qualifier("employeeMapper") EntityDTOMapper<Employee, EmployeeDTO> employeeDTOEntityDTOMapper,
+                                      DemoNamedParameterJdbcRepository demoNamedParameterJdbcRepository,
+                                      DemoEntityManagerRepository demoEntityManagerRepository) {
+        this.demoJdbcTemplateRepository = demoJdbcTemplateRepository;
+        this.employeeDTOEntityDTOMapper = employeeDTOEntityDTOMapper;
+        this.demoNamedParameterJdbcRepository = demoNamedParameterJdbcRepository;
+        this.demoEntityManagerRepository = demoEntityManagerRepository;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "procedure/demo")
+    public EmployeeListResponse getEmployees(
+            @RequestParam(name = "employeeName", required = false) String employeeName,
+            @RequestParam(name = "department", required = false) Department department,
+            @RequestParam(name = "pageNo", defaultValue = "1") int pageNumber
+    ) {
+        PageResponse<Employee> employeePage = demoJdbcTemplateRepository.fetchEmployeesWithSimpleJdbc(pageNumber - 1, 5, employeeName, department != null ? department.name() : null);
+
+        List<EmployeeDTO> employeeDTOS = employeePage.getContent().stream()
+                .map(employeeDTOEntityDTOMapper.toDTO())
+                .toList();
+
+        return EmployeeListResponse.builder()
+                .employeeDTOS(employeeDTOS)
+                .currentPageNumber(employeePage.getPage() + 1)
+                .totalPage(employeePage.getTotalPages())
+                .build();
+
+
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "batch/demo")
+    public ResponseEntity<Void> addEmployee(@RequestBody List<EmployeeDTO> employeeDTO) {
+        List<Employee> employees = employeeDTO.stream()
+                .map(employeeDTOEntityDTOMapper.toEntity())
+                .toList();
+
+        demoNamedParameterJdbcRepository.insertEmployees1(employees);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "batch/demo1")
+    public ResponseEntity<List<EmployeeDTO>> addEmployee1(@RequestBody List<EmployeeDTO> employeeDTO) {
+        List<Employee> employees = employeeDTO.stream()
+                .map(employeeDTOEntityDTOMapper.toEntity())
+                .toList();
+
+        employees = demoNamedParameterJdbcRepository.insertEmployees2(employees);
+
+        List<EmployeeDTO> employeeDTOS = employees.stream()
+                .map(employeeDTOEntityDTOMapper.toDTO())
+                .toList();
+
+        return new ResponseEntity<>(employeeDTOS, HttpStatus.OK);
+
+    }
+
+    @GetMapping(path = "demo/findByEmployeeId/{employeeId}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable("employeeId") long employeeId) {
+        Employee employee = demoEntityManagerRepository.findByEmployeeIdUsingJpql(employeeId);
+
+        log.info("fetch completed");
+
+        EmployeeDTO employeeDTO = employeeDTOEntityDTOMapper.toDTO().apply(employee);
+
+        return new ResponseEntity<>(employeeDTO, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "find/all")
+    public ResponseEntity<List<EmployeeDTO>> findAll() {
+        List<Employee> employees = demoEntityManagerRepository.findAll6();
+
+        List<EmployeeDTO> employeeDTOS = employees.stream()
+                .map(employeeDTOEntityDTOMapper.toDTO())
+                .toList();
+
+        return new ResponseEntity<>(employeeDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "findByEmployeeName")
+    public ResponseEntity<List<EmployeeDTO>> findByEmployeeName(@RequestParam("employeeName") String employeeName) {
+        List<Employee> employees = demoEntityManagerRepository.findByEmployeeName1(employeeName);
+
+        List<EmployeeDTO> employeeDTOS = employees.stream()
+                .map(employeeDTOEntityDTOMapper.toDTO())
+                .toList();
+
+        return new ResponseEntity<>(employeeDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "find/all1")
+    public ResponseEntity<List<EmployeeDTO>> findAll1() {
+        List<Employee> employees = demoEntityManagerRepository.findAll4();
+
+        List<EmployeeDTO> employeeDTOS = employees.stream()
+                .map(
+                        employee -> EmployeeDTO.builder()
+                                .employeeId(employee.getEmployeeId())
+                                .employeeName(employee.getEmployeeName())
+                                .build()
+
+                )
+                .toList();
+
+        return new ResponseEntity<>(employeeDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "findByEmployeeNameAndDepartment")
+    public ResponseEntity<List<EmployeeDTO>> findByEmployeeNameAndDepartment(@RequestParam("employeeName") String employeeName,
+                                                                             @RequestParam("department") String department) {
+        List<Employee> employees = demoEntityManagerRepository.findByEmployeeNameAndDepartment(employeeName, department);
+
+        List<EmployeeDTO> employeeDTOS = employees.stream()
+                .map(employeeDTOEntityDTOMapper.toDTO())
+                .toList();
+
+        return new ResponseEntity<>(employeeDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "find")
+    public ResponseEntity<List<EmployeeDTO>> find(@RequestParam("employeeName") String employeeName,
+                                                  @RequestParam("department") String department,
+                                                  @RequestParam("age") int age,
+                                                  @RequestParam("employeeNameContain") String employeeNameContain) {
+        List<Employee> employees = demoEntityManagerRepository.find(employeeName, department, age, employeeNameContain);
+
+        List<EmployeeDTO> employeeDTOS = employees.stream()
+                .map(employeeDTOEntityDTOMapper.toDTO())
+                .toList();
+
+        return new ResponseEntity<>(employeeDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "findDepartmentWithMinEmployees")
+    public ResponseEntity<List<DepartmentCount>> findDepartmentWithMinEmployees(@RequestParam("minCount") long minCount) {
+        List<DepartmentCount> departmentCounts = demoEntityManagerRepository.findDepartmentWithMinEmployees(minCount);
+
+        return new ResponseEntity<>(departmentCounts, HttpStatus.OK);
+    }
+
+    @Transactional
+    @PutMapping("updateEmployeeDemo")
+    public EmployeeDTO updateEmployeeDemo(@RequestBody EmployeeDTO employeeDTO) {
+        Optional<Employee> employeeOptional = demoEntityManagerRepository.findByEmployeeIdForUpdate(employeeDTO.getEmployeeId());
+
+        if (employeeOptional.isEmpty()) {
+            throw new EmployeeNotFoundException(employeeDTO.getEmployeeId());
+        }
+
+        Employee employee = employeeOptional.get();
+
+        // Employee employeeEntity = employeeOptional.get();
+
+        // Employee employee = new Employee();
+        //employee.setEmployeeId(employeeEntity.getEmployeeId());
+        //employee.setJoiningDate(employeeEntity.getJoiningDate());
+
+        employee.setEmployeeName(employeeDTO.getEmployeeName());
+        employee.setAge(employeeDTO.getAge());
+        employee.setDepartment(employeeDTO.getDepartment().name());
+
+        employee = demoEntityManagerRepository.updateEmployee(employee);
+
+        return employeeDTOEntityDTOMapper.toDTO().apply(employee);
+    }
+
+    @Transactional
+    @PutMapping("mergeMethodDemo")
+    public EmployeeDTO mergeMethodDemo(@RequestBody EmployeeDTO employeeDTO) {
+
+        Employee employee = employeeDTOEntityDTOMapper.toEntity().apply(employeeDTO);
+
+        if (employeeDTO.getEmployeeId() == 0) {
+            employee.setEmployeeId(null);
+        }
+
+        employee = demoEntityManagerRepository.updateEmployee1(employee);
+
+
+        return employeeDTOEntityDTOMapper.toDTO().apply(employee);
+    }
+
+
+}
---
+++ How to Use EntityManager to Update Data in Spring Boot​/employee-service/employee-dao/src/main/java/com/gyan/darpan/dao/repository/jdbc/EmployeeEntityManagerRepository.java
@@ -0,0 +1,238 @@
+package com.gyan.darpan.dao.repository.jdbc;
+
+import com.gyan.darpan.dao.entity.Employee;
+import com.gyan.darpan.dao.entity.PageResponse;
+import com.gyan.darpan.dao.repository.EmployeeRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository("employeeEntityManagerRepository")
+@Slf4j
+public class EmployeeEntityManagerRepository implements EmployeeRepository {
+
+    private EntityManager entityManager;
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public Employee saveEmployee(Employee employee) {
+        try {
+            log.info("EmployeeEntityManagerRepository::saveEmployee");
+            log.info("Entity Manager : {} ", entityManager.toString());
+            entityManager.persist(employee);
+        } catch (Exception e) {
+            log.error("Exception Occur :", e);
+            throw e;
+        }
+        return employee;
+    }
+
+    @Override
+    public Optional<Employee> findByEmployeeId(long employeeId) {
+        log.info("EmployeeEntityManagerRepository::findByEmployeeId");
+        Employee employee = entityManager.find(Employee.class, employeeId);
+
+        return Optional.ofNullable(employee);
+    }
+
+    @Override
+    public Optional<Employee> findByEmployeeIdForUpdate(long employeeId) {
+        //Approach1 start
+    /*
+        String query = "Select emp from Employee emp where emp.employeeId=:employeeId";
+        Employee employee = null;
+        try {
+            employee = entityManager.createQuery(query, Employee.class)
+                    .setParameter("employeeId", employeeId)
+                    .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+                    .getSingleResult();
+        } catch (NoResultException noResultException) {
+            log.error("findByEmployeeIdForUpdate::no record found ::{}", noResultException.getMessage());
+        }
+    */
+
+        //Approach1 end
+
+        //Approach2 start
+/*
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> root = criteriaQuery.from(Employee.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("employeeId"), employeeId));
+
+        Employee employee = null;
+        try {
+            employee = entityManager.createQuery(criteriaQuery)
+                    .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+                    .getSingleResult();
+        } catch (NoResultException noResultException) {
+            log.error("findByEmployeeIdForUpdate::no record found ::{}", noResultException.getMessage());
+        }
+*/
+
+        //Approach2 end
+
+        //Approach3 start
+        Employee employee = entityManager.find(Employee.class, employeeId, LockModeType.PESSIMISTIC_WRITE);
+        //Approach3 end
+
+        return Optional.ofNullable(employee);
+    }
+
+    @Override
+    public PageResponse<Employee> fetchEmployees(int pageNumber, int pageSize, String employeeName, String department) {
+//        List<String> conditions = new ArrayList<>();
+//        Map<String, Object> parameterSource = new HashMap<>();
+//
+//        if (employeeName != null && !employeeName.isBlank()) {
+//            conditions.add("emp.employeeName= :employeeName ");
+//            parameterSource.put("employeeName", employeeName);
+//        }
+//
+//        if (department != null && !department.isBlank()) {
+//            conditions.add("emp.department=:department ");
+//            parameterSource.put("department", department);
+//        }
+//
+//        String query = "Select emp from Employee emp ";
+//        String countQuery = "Select count(emp) from Employee emp";
+//
+//        if (!conditions.isEmpty()) {
+//            String condition = " WHERE " + String.join(" AND ", conditions);
+//            query = query + condition;
+//            countQuery = countQuery + condition;
+//        }
+//
+//        TypedQuery<Long> typeCountQuery = entityManager.createQuery(countQuery, Long.class);
+//
+//        parameterSource.forEach(typeCountQuery::setParameter);
+//
+//        long totalEmployees = typeCountQuery.getSingleResult();
+//
+//        int offset = pageNumber * pageSize;
+//
+//        TypedQuery<Employee> typedQuery = entityManager.createQuery(query, Employee.class);
+//
+//        parameterSource.forEach(typedQuery::setParameter);
+//
+//        typedQuery.setFirstResult(offset);
+//        typedQuery.setMaxResults(pageSize);
+//
+//
+//        List<Employee> employees = typedQuery.getResultList();
+//
+//        return new PageResponse<>(employees, pageNumber, pageSize, totalEmployees);
+
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> root = criteriaQuery.from(Employee.class);
+
+        List<Predicate> predicates = getPredicates(employeeName, department, criteriaBuilder, root);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(predicates.toArray(new Predicate[0]));
+
+        int offset = pageNumber * pageSize;
+
+        List<Employee> employees = entityManager.createQuery(criteriaQuery)
+                .setFirstResult(offset)
+                .setMaxResults(pageSize)
+                .getResultList();
+
+
+        CriteriaQuery<Long> countQuery = criteriaBuilder.createQuery(Long.class);
+        Root<Employee> countRoot = countQuery.from(Employee.class);
+
+        List<Predicate> countPredicate = getPredicates(employeeName, department, criteriaBuilder, countRoot);
+
+        countQuery.select(criteriaBuilder.count(countRoot));
+        countQuery.where(countPredicate.toArray(new Predicate[0]));
+
+        long totalEmployees = entityManager.createQuery(countQuery)
+                .getSingleResult();
+
+        return new PageResponse<>(employees, pageNumber, pageSize, totalEmployees);
+    }
+
+    private List<Predicate> getPredicates(String employeeName, String department, CriteriaBuilder criteriaBuilder, Root<Employee> root) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (employeeName != null && !employeeName.isBlank()) {
+            predicates.add(criteriaBuilder.equal(root.get("employeeName"), employeeName));
+        }
+
+        if (department != null && !department.isBlank()) {
+            predicates.add(criteriaBuilder.equal(root.get("department"), department));
+        }
+        return predicates;
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        //Approach1 start
+       /* String query = "Update Employee emp set emp.employeeName=:employeeName , emp.age=:age , emp.department=:department where emp.employeeId=:employeeId";
+
+
+        int updateCount = entityManager.createQuery(query)
+                .setParameter("employeeName", employee.getEmployeeName())
+                .setParameter("age", employee.getAge())
+                .setParameter("department", employee.getDepartment())
+                .setParameter("employeeId", employee.getEmployeeId())
+                .executeUpdate();
+
+        */
+        //Approach1 end
+
+        //Approach2 start
+      /*
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaUpdate<Employee> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(Employee.class);
+        Root<Employee> root = criteriaUpdate.getRoot();
+
+        criteriaUpdate.set(root.get("employeeName"), employee.getEmployeeName());
+        criteriaUpdate.set(root.get("age"), employee.getAge());
+        criteriaUpdate.set(root.get("department"), employee.getDepartment());
+
+        criteriaUpdate.where(criteriaBuilder.equal(root.get("employeeId"), employee.getEmployeeId()));
+
+        int updateCount = entityManager.createQuery(criteriaUpdate)
+                .executeUpdate();
+
+       */
+
+        //Approach2 end
+
+        //Approach3 start
+
+        return entityManager.merge(employee);
+
+        //Approach3 end
+
+        //return employee;
+    }
+
+    @Override
+    public int deleteEmployee(long employeeId) {
+        return 0;
+    }
+}
