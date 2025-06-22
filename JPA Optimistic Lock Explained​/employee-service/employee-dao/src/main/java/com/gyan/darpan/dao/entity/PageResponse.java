96dda3389a01da622cfd3a6943d78458fa9827bb
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-06-22 21:56:27 +0530

    Add files via upload

---
+++ JPA Optimistic Lock Explained​/employee-service/employee-dao/src/main/java/com/gyan/darpan/dao/entity/PageResponse.java
@@ -0,0 +1,22 @@
+package com.gyan.darpan.dao.entity;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PageResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+
+    public PageResponse(List<T> content, int page, int size, long totalElements) {
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = (int) Math.ceil((double) totalElements / size);
+    }
+}
