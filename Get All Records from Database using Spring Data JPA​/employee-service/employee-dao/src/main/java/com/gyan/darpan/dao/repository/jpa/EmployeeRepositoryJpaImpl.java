e39e8668464066905bdeefba1b07fabe43c9a536
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-07-19 12:59:43 +0530

    Add files via upload

---
+++ Get All Records from Database using Spring Data JPA​/employee-service/employee-dao/src/main/java/com/gyan/darpan/dao/repository/jpa/EmployeeRepositoryJpaImpl.java
@@ -0,0 +1,54 @@
+package com.gyan.darpan.dao.repository.jpa;
+
+import com.gyan.darpan.dao.entity.Employee;
+import com.gyan.darpan.dao.entity.PageResponse;
+import com.gyan.darpan.dao.repository.EmployeeRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository("employeeRepositoryJpaImpl")
+public class EmployeeRepositoryJpaImpl implements EmployeeRepository {
+
+    private final EmployeeJpaRepository employeeJpaRepository;
+
+    public EmployeeRepositoryJpaImpl(EmployeeJpaRepository employeeJpaRepository) {
+        this.employeeJpaRepository = employeeJpaRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeJpaRepository.save(employee);
+    }
+
+    @Override
+    public Optional<Employee> findByEmployeeId(long employeeId) {
+        return employeeJpaRepository.findById(employeeId);
+    }
+
+    @Override
+    public Optional<Employee> findByEmployeeIdForUpdate(long employeeId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public PageResponse<Employee> fetchEmployees(int pageNumber, int pageSize, String employeeName, String department) {
+        List<Employee> employees = employeeJpaRepository.findAll();
+
+        long totalEmployees = employees.size();
+
+        return new PageResponse<>(employees, pageNumber, pageSize, totalEmployees);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public int deleteEmployee(long employeeId) {
+        return 0;
+    }
+}
