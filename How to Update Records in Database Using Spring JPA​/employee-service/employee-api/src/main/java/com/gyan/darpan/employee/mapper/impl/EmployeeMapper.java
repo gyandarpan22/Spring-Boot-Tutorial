f57a5b91a34553b731b7e81670e178e038d50f66
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-11-09 22:03:27 +0530

    Add files via upload

---
+++ How to Update Records in Database Using Spring JPA​/employee-service/employee-api/src/main/java/com/gyan/darpan/employee/mapper/impl/EmployeeMapper.java
@@ -0,0 +1,37 @@
+package com.gyan.darpan.employee.mapper.impl;
+
+import com.gyan.darpan.dao.entity.Employee;
+import com.gyan.darpan.employee.enums.Department;
+import com.gyan.darpan.employee.mapper.EntityDTOMapper;
+import com.gyan.darpan.employee.model.EmployeeDTO;
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+import java.util.function.Function;
+
+@Component("employeeMapper")
+public class EmployeeMapper implements EntityDTOMapper<Employee, EmployeeDTO> {
+    @Override
+    public Function<Employee, EmployeeDTO> toDTO() {
+        return employee -> EmployeeDTO.builder()
+                .employeeId(employee.getEmployeeId())
+                .employeeName(employee.getEmployeeName())
+                .age(employee.getAge())
+                .joiningDate(employee.getJoiningDate().toLocalDate())
+                .department(Department.valueOf(employee.getDepartment()))
+                .salary(employee.getSalary())
+                .build();
+    }
+
+    @Override
+    public Function<EmployeeDTO, Employee> toEntity() {
+        return employee -> Employee.builder()
+                .employeeId(employee.getEmployeeId())
+                .employeeName(employee.getEmployeeName())
+                .age(employee.getAge())
+                .joiningDate(Date.valueOf(employee.getJoiningDate()))
+                .department(employee.getDepartment().name())
+                .salary(employee.getSalary())
+                .build();
+    }
+}
---
+++ How to Update Records in Database Using Spring JPA​/employee-service/employee-dao/src/main/java/com/gyan/darpan/dao/repository/jpa/EmployeeRepositoryJpaImpl.java
@@ -0,0 +1,79 @@
+package com.gyan.darpan.dao.repository.jpa;
+
+import com.gyan.darpan.dao.entity.Employee;
+import com.gyan.darpan.dao.entity.PageResponse;
+import com.gyan.darpan.dao.repository.EmployeeRepository;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository("employeeRepositoryJpaImpl")
+public class EmployeeRepositoryJpaImpl implements EmployeeRepository {
+
+    private final EmployeeJpaRepository employeeJpaRepository;
+
+    public EmployeeRepositoryJpaImpl(EmployeeJpaRepository employeeJpaRepository) {
+        this.employeeJpaRepository = employeeJpaRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeJpaRepository.save(employee);
+    }
+
+    @Override
+    public Optional<Employee> findByEmployeeId(long employeeId) {
+        return employeeJpaRepository.findById(employeeId);
+    }
+
+    @Override
+    public Optional<Employee> findByEmployeeIdForUpdate(long employeeId) {
+        return employeeJpaRepository.findByEmployeeIdForUpdate(employeeId);
+    }
+
+    @Override
+    public PageResponse<Employee> fetchEmployees(int pageNumber, int pageSize, String employeeName, String department) {
+        Specification<Employee> spec = (root, criteriaQuery, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (employeeName != null && !employeeName.isBlank()) {
+                predicates.add(criteriaBuilder.equal(root.get("employeeName"), employeeName));
+            }
+
+            if (department != null && !department.isBlank()) {
+                predicates.add(criteriaBuilder.equal(root.get("department"), department));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+        Page<Employee> employeePage = employeeJpaRepository.findAll(spec, pageable);
+
+        List<Employee> employees = employeePage.getContent();
+
+        long totalEmployees = employeePage.getTotalElements();
+
+        return new PageResponse<>(employees, pageNumber, pageSize, totalEmployees);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        return employeeJpaRepository.save(employee);
+    }
+
+    @Override
+    public int deleteEmployee(long employeeId) {
+        return 0;
+    }
+}
