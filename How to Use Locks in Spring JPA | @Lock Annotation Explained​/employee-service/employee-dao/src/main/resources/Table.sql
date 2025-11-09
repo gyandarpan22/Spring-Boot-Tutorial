4769492a18b0092ee5cd485add9c8318eb47f382
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2025-11-09 20:57:22 +0530

    Add files via upload

---
+++ How to Use Locks in Spring JPA | @Lock Annotation Explained​/employee-service/employee-dao/src/main/resources/Table.sql
@@ -0,0 +1,23 @@
+create table employee(
+    employee_id    bigint auto_increment primary key,
+    employee_name  varchar(100) NOT NULL,
+    age            int,
+    joining_date   date not null,
+    department     enum('DEVELOPER','QA','HR') not null,
+    salary         double
+    created_date   timestamp not null default current_timestamp,
+    modified_date  timestamp not null default current_timestamp on update current_timestamp
+)
+
+
+create table student(
+    student_id  bigint auto_increment primary key,
+    student_name varchar(100) NOT NULL,
+    version  bigint
+)
+
+Alter table student add version bigint default 0
+
+ALTER TABLE employee ADD COLUMN salary DOUBLE;
+
+UPDATE employee SET salary = FLOOR(10000 + (RAND() * (500000 - 10000)));
