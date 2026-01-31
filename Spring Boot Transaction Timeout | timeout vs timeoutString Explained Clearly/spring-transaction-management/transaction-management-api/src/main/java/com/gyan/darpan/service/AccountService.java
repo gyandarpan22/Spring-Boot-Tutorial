b80fc6d041501af7395c1151609325b4bd3fe055
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2026-01-31 19:56:15 +0530

    Add files via upload

---
+++ Spring Boot Transaction Timeout | timeout vs timeoutString Explained Clearly/spring-transaction-management/transaction-management-api/src/main/java/com/gyan/darpan/service/AccountService.java
@@ -0,0 +1,40 @@
+package com.gyan.darpan.service;
+
+import com.gyan.darpan.dto.TransferAmountDto;
+import com.gyan.darpan.entity.Account;
+import com.gyan.darpan.respository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Transactional(rollbackFor = {Exception.class}, timeoutString = "${account.transfer-amount.timeout}")
+    public void transferAmount(TransferAmountDto transferAmountDto) throws Exception {
+        Account fromAccount = accountRepository.findById(transferAmountDto.getFromAccountId()).get();
+        Account toAccount = accountRepository.findById(transferAmountDto.getToAccountId()).get();
+
+        if (fromAccount.getAmount().compareTo(transferAmountDto.getAmount()) < 0) {
+            throw new RuntimeException("Insufficient Amount");
+        }
+
+        fromAccount.setAmount(fromAccount.getAmount().subtract(transferAmountDto.getAmount()));
+        accountRepository.save(fromAccount);
+
+        toAccount.setAmount(toAccount.getAmount().add(transferAmountDto.getAmount()));
+        accountRepository.save(toAccount);
+
+        try {
+            Thread.sleep(1500);
+        } catch (Exception e) {
+
+        }
+    }
+}
