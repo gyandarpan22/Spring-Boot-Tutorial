9f4503d9cf81cfb59665f85e7d614fff93e2532e
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2026-02-22 16:24:43 +0530

    Delete enterprise-management-system directory

--- enterprise-management-system/ems-dao/src/main/java/com/gyan/darpan/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gyan.darpan.repository;
-
-import com.gyan.darpan.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
